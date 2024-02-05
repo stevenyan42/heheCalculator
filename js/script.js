89b6702f36a0cb848ae80fbd73a702a22c4eb208
stevenyan <steven.yan42@gmail.com>
2024-02-04 23:28:38 -0800

    full calculator functionality

--- js/script.js
+++ js/script.js
@@ -1,99 +1,201 @@
-let num1 = "";
-let num2 = "";
-let operator = null;
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText
+        this.clearDisplay();
+    }
 
-let num1Filled = false;
-let num2Filled = false;
+    setDisplay() {
+        this.currentOperationText.textContent = 
+            this.getDisplayNum(this.currentOperation);
+        if (this.operator != null) {
+            this.previousOperationText.textContent = 
+            `${this.getDisplayNum(this.previousOperation)} ${this.operator}`;
+        } else {
+            this.previousOperationText.textContent = '';
+        }
+    }
 
-let numbers = document.querySelectorAll(".number");
-let operators = document.querySelectorAll(".operator");
-let equal = document.querySelector(".equal");
-let display = document.querySelector(".display");
+    getDisplayNum(num){
+        const stringNum = num.toString();
+        const integerDigits = parseFloat(stringNum.split('.')[0]);
+        const decimalDigits = stringNum.split('.')[1]
+        let integerDisplay;
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+            console.log(integerDisplay)
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0 });
+        }
+        if (decimalDigits != undefined) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
 
-function numberHandler(event) {
-    if (!num1Filled) {
-        num1 += event.target.value;
-        setDisplay(num1);
-        console.log(num1);
-    } else if (!num2Filled) {
-        num2 += event.target.value;
-        setDisplay(num2);
-        console.log(num2);
+    appendNumber(num) {
+        if (num ==='.' && this.currentOperation.includes('.')) return
+        this.currentOperation += num;
+    }
+
+    chooseOperator(operator) {
+        if (this.currentOperation === '') return
+        if (this.previousOperation !==''){
+            this.operate();
+        }
+        this.operator = operator;
+        this.previousOperation = this.currentOperation;
+        this.currentOperation = '';
+    }
+
+    clearDisplay() {
+        this.currentOperation = '';
+        this.previousOperation = '';
+        this.operator = undefined;
+    }
+
+    operate() {
+        let computation
+        const prev = parseFloat(this.previousOperation);
+        const current = parseFloat(this.currentOperation);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operator) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '/':
+                computation = prev / current;
+                break;
+            default:
+                return   
+        }
+        this.currentOperation = computation;
+        this.operator = undefined;
+        this.previousOperation = '';
+    }   
+
+    delete() {
+        this.currentOperation = this.currentOperation.slice(0,-1);
     }
 }
 
-function operatorHandler(event) {
-    //set operator = value
-    operator = event.target.value;
-    console.log(operator);
-    if (!num1Filled) {
-        num1Filled = true;
-    } else {
-        //if two numbers were already filled
-        num1 = operate(Number(num1), Number(num2), operator);
-        num2 = "";
-        setDisplay(num1);
-        num1Filled = true;
-    }
-}
 
-function setDisplay(num) {
-    display.textContent = num;
-}
 
-function equalHandler() {
-    num1 = operate(Number(num1), Number(num2), operator);
-    num2 = "";
-    num1Filled = false;
-    setDisplay(num1);
-}
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const display = document.querySelector(".display");
+const clearButton = document.querySelector(".clear");
+const delButton = document.querySelector('.del')
+const previousOperationText = document.querySelector(".previousDisplay")
+const currentOperationText = document.querySelector(".currentDisplay")
+
+const calculator = new Calculator(previousOperationText, currentOperationText);
 
 numbers.forEach(number => {
-    number.addEventListener("click", numberHandler);
+    number.addEventListener("click", () => {
+        calculator.appendNumber(number.value);
+        calculator.setDisplay();
+    })
 });
 
-operators.forEach(number => {
-    number.addEventListener("click", operatorHandler);
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        calculator.chooseOperator(operator.value);
+        calculator.setDisplay();
+    })
 });
 
-equal.addEventListener("click", equalHandler)
+equal.addEventListener("click", () => {
+    calculator.operate();
+    calculator.setDisplay();
+});
+
+clearButton.addEventListener("click", () => {
+    calculator.clearDisplay();
+    calculator.setDisplay();
+});
+
+delButton.addEventListener("click", () => {
+    calculator.delete();
+    calculator.setDisplay();
+});
+
+// function equalHandler() {
+//     if (resetScreen == true) {
+//         return
+//     }
+//     num1 = operate(Number(num1), Number(num2), operator);
+//     num2 = "";
+//     operator = "";
+//     num1Filled = false;
+//     resetScreen = true;
+//     setDisplay(num1);
+// }
+
+// function clearHandler() {
+//     num1 = "";
+//     num2 = "";
+//     operator = "";
+//     num1Filled = false;
+//     resetScreen = false;
+//     setDisplay("0");
+// }
 
 
-function add(a,b) {
-    return a + b;
-}
+// equal.addEventListener("click", equalHandler);
+// clearButton.addEventListener("click", clearHandler);
 
-function subtract(a,b) {
-    return a - b;
-}
 
-function multiply(a,b) {
-    return a * b;
-}
+// function add(a,b) {
+//     return a + b;
+// }
 
-function divide(a,b) {
-    return a / b;
-}
+// function subtract(a,b) {
+//     return a - b;
+// }
 
-function operate(num1, num2, operator) {
-    let result = 0;
+// function multiply(a,b) {
+//     return a * b;
+// }
 
-    switch(operator) {
-        case "+":
-            result = add(num1, num2);
-            break;
-        case "-":
-            result = subtract(num1, num2);
-            break;
-        case "*":
-            result = multiply(num1, num2);
-            break;
-        case "/":
-            result = divide(num1, num2);
-            break;
-        default:
-            result = 0;
-    }
+// function divide(a,b) {
+//     if (b == 0) {
+//         return "ERROR"
+//     } else {
+//         return a / b;
+//     }
+    
+// }
 
-    return result;
-}
+// function operate(num1, num2, operator) {
+//     let result = 0;
+
+//     switch(operator) {
+//         case "+":
+//             result = add(num1, num2);
+//             break;
+//         case "-":
+//             result = subtract(num1, num2);
+//             break;
+//         case "*":
+//             result = multiply(num1, num2);
+//             break;
+//         case "/":
+//             result = divide(num1, num2);
+//             break;
+//         default:
+//             result = "";
+//     }
+
+//     return result;
+// }
+
